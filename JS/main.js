795442ef9343bfb8ae01f9b79ac6b3a12cc57c4d
adidhakane <132973340+adidhakane@users.noreply.github.com>
2025-02-12 19:38:24 +0530

    Delete JS directory

--- JS/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    const slides = $('.slides');
-    const totalSlides = $('.slide').length;
-    
-    // Set the width of the slides container dynamically
-    slides.css('width', totalSlides * 100 + '%');
-    
-    // No additional JS is required for the sliding and repeat behavior
-});
